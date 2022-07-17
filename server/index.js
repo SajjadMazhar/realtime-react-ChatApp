5b78cdf2cf0d276f9314440a0ca5e08f436fb50a
SajjadMazhar <sajjad21@navgurukul.org>
2022-07-17 16:20:34 +0530

    two way communication done

--- server/index.js
+++ server/index.js
@@ -1,6 +1,9 @@
 const express = require("express")
 const socketio = require("socket.io")
 const http = require("http")
+const {
+    addUser, removeUser, getUser, getUsersInRoom
+}= require("./users")
 
 const PORT = process.env.PORT || 5000
 
@@ -17,15 +20,20 @@ app.use(require('cors')())
 app.use(require("./routes/router"))
 
 io.on('connection', (socket)=>{
-    console.log("connection has been made")
 
     socket.on('join', ({name, room})=>{
-        console.log(name, room)
+        // const {error, user} = addUser({id:socket.id, name, room})
+        socket.join(room)
+    })
+    
+    socket.on("msg", ({text, name, room}, callback)=>{
+        console.log({text, name, room})
+        socket.broadcast.to(room).emit("received", {text, name})
+        callback()
     })
 
-    socket.on("disconnect", ()=>{
-        console.log("user just left the chat")
-    })
+    
+
 })
 
 
